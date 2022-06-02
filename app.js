d80507045112351885930e71e301d46f3c664b89
Avishka Indula <avishkainduladev@gmail.com>
2022-06-02 10:15:33 +0530

    managing data with unique IDs and uuid package

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const fs = require("fs");
 const path = require("path");
 
 const express = require("express");
+const uuid = require("uuid");
+// uuid is a JS package that generates unique IDs
+// uuid is an object
 
 const app = express();
 
@@ -13,6 +16,31 @@ app.use(express.urlencoded({ extended: false }));
 
 app.get("/", function (req, res) {
   res.render("index");
+});
+
+app.get("/recommend", function (req, res) {
+  res.render("recommend");
+});
+
+app.post("/recommend", function (req, res) {
+  const restaurant = req.body;
+
+  restaurant.restid = uuid.v4();
+  // restid is a property that doesn't exist on restaurant object yet.
+  // But right after we access a property that doesn't exist, JS will automatically create that property for us.
+  // .v4() method will dive us unique ids.
+  // Those ids are actually strings.
+  // Now every restaurant we add will have a unique id.
+
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  storedRestaurants.push(restaurant);
+
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+  res.redirect("/confirm");
 });
 
 app.get("/restaurants", function (req, res) {
@@ -27,31 +55,14 @@ app.get("/restaurants", function (req, res) {
   });
 });
 
-app.get("/restaurants/:id", function (req, res) {
-  const restaurantId = req.params.id;
-  // .id is the same on :id
+app.get("/restaurants/:restid", function (req, res) {
+  const restaurantId = req.params.restid;
+  // .restid is the same on :restid
   res.render("restaurant-detail", { rid: restaurantId });
 });
+// This is the route that is responsible for the restaurant-detail page.
 // This is how we add a Dynamic route to our URL
-// "id" is a dynamic placeholder. We can also use another name instead of id.
-
-app.get("/recommend", function (req, res) {
-  res.render("recommend");
-});
-
-app.post("/recommend", function (req, res) {
-  const restaurant = req.body;
-  const filePath = path.join(__dirname, "data", "restaurants.json");
-
-  const fileData = fs.readFileSync(filePath);
-  const storedRestaurants = JSON.parse(fileData);
-
-  storedRestaurants.push(restaurant);
-
-  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
-
-  res.redirect("/confirm");
-});
+// "restid" is a dynamic placeholder. We can also use another name instead of restid.
 
 app.get("/confirm", function (req, res) {
   res.render("confirm");
